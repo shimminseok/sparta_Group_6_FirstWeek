77068b702a3b12254d3f3fccbe89d819f84c6fc8
shimminseok <61370379+shimminseok@users.noreply.github.com>
2025-04-07 21:31:45 +0900

    Revert "수정 : Board 클래스 3, 6, 9"
    This reverts commit a89ed224dbd64520fd470bc6fca9ddb1ee669e7f.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,27 +1,23 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
-
 public class Board : MonoBehaviour
 {
     public Transform cards;
     public GameObject card;
-    public int vertical = 0;
 
     void Start()
     {
-        int[] arr = CreateCard(vertical);  // 0~8 카드 두 개씩
+        int[] arr = CreateCard(8);  // {0,0,1,1, ...,7,7};
 
-        // 랜덤하게 섞기
-        arr = arr.OrderBy(x => Random.Range(0f, 1f)).ToArray();
+        arr = arr.OrderBy(x => Random.Range(0, arr.Last())).ToArray();
 
         for (int i = 0; i < arr.Length; i++)
         {
             GameObject go = Instantiate(card, cards);
 
-            float x = (i % vertical) * 1.6f;    // 가로 9칸
-            float y = -(i / vertical) * 2.6f;   // 세로 2줄 (0 또는 1)
-
+            float x = (i % 4) * 1.4f;
+            float y = (i / 4) * 1.4f;
             go.transform.localPosition = new Vector2(x, y);
 
             float x = (i % 3) * 1.4f;
@@ -32,6 +28,7 @@ public class Board : MonoBehaviour
 
         GameManager.Instance.cardCount = arr.Length;
     }
+
 
     int[] CreateCard(int _cnt)
     {
@@ -45,4 +42,3 @@ public class Board : MonoBehaviour
         return arr;
     }
 }
-
