a89ed224dbd64520fd470bc6fca9ddb1ee669e7f
Leekiahn <kiahn2000@gmail.com>
2025-04-07 21:16:44 +0900

    수정 : Board 클래스 3, 6, 9

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
+
 public class Board : MonoBehaviour
 {
     public Transform cards;
     public GameObject card;
+    public int vertical = 0;
 
     void Start()
     {
-        int[] arr = CreateCard(8);  // {0,0,1,1, ...,7,7};
+        int[] arr = CreateCard(vertical);  // 0~8 카드 두 개씩
 
-        arr = arr.OrderBy(x => Random.Range(0, arr.Last())).ToArray();
+        // 랜덤하게 섞기
+        arr = arr.OrderBy(x => Random.Range(0f, 1f)).ToArray();
 
         for (int i = 0; i < arr.Length; i++)
         {
             GameObject go = Instantiate(card, cards);
 
-            float x = (i % 4) * 1.4f;
-            float y = (i / 4) * 1.4f;
+            float x = (i % vertical) * 1.6f;    // 가로 9칸
+            float y = -(i / vertical) * 2.6f;   // 세로 2줄 (0 또는 1)
+
             go.transform.localPosition = new Vector2(x, y);
 
             go.GetComponent<Card>().Setting(arr[i]);
@@ -25,7 +29,6 @@ public class Board : MonoBehaviour
 
         GameManager.Instance.cardCount = arr.Length;
     }
-
 
     int[] CreateCard(int _cnt)
     {
@@ -39,3 +42,4 @@ public class Board : MonoBehaviour
         return arr;
     }
 }
+
